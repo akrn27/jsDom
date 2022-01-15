77057498e6f40fdf1fa5215186e0196c5e32561c
Akrn27 <akurnia755@gmail.com>
2022-01-15 16:31:45 +0700

    create getElementsByTagName

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 // DOM SELECTION
-// document.getElementById()
+// document.getElementById() -> element
 const judul = document.getElementById('judul');
+judul.style.color = "red";
+judul.style.backgroundColor = "black";
+judul.innerHTML = "Muhammad Andika";
+
+// document.getElementsByTagName() -> HTMLCollections
+const p = document.getElementsByTagName('p');
