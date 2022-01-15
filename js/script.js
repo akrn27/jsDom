39eee54c08184e7d964182e4effa16f92ec59add
Akrn27 <akurnia755@gmail.com>
2022-01-15 17:15:28 +0700

    create getElementsByClassName()

--- js/script.js
+++ js/script.js
@@ -5,9 +5,17 @@ judul.style.color = "red";
 judul.style.backgroundColor = "black";
 judul.innerHTML = "Muhammad Andika";
 
-// document.getElementsByTagName() -> HTMLCollections
-const p = document.getElementsByTagName('p');
 
-for(let i = 0; i <= p.length; i++){
-    p[i].style.backgroundColor = "blue";
+// document.getElementsByTagName() -> HTMLCollection
+const p = document.getElementsByTagName('p');
+for(let i = 0; i < p.length; i++){
+    p[i].style.backgroundColor = "lightBlue";
 }
+
+const h1 = document.getElementsByTagName('h1')[0];
+h1.style.fontSize = '50px';
+
+
+// document.getElementsByClassName() -> HTMLCollection
+const p1 = document.getElementsByClassName('p1');
+p1[0].innerHTML = 'andikaaaaaaaaaaa';
