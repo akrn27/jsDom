05e645c2dc9fd2a631185cb933db7dd5dab250b1
Akrn27 <akurnia755@gmail.com>
2022-01-17 13:54:37 +0700

    classlist js

---
+++ js/jsDom5.js
@@ -0,0 +1,14 @@
+// const judul = document.getElementById('judul');
+// judul.innerHTML = 'andika';
+
+// const sectionA = document.querySelector('section#a');
+// sectionA.innerHTML = 
+// '<p>Hello Guys</p> <h1>hiadishdia</h1>';
+
+// const judul = document.getElementById('judul');
+// judul.style.color = 'blue';
+
+const judul = document.getElementsByTagName('h1')[0];
+const a = document.querySelector('section#a a');
+
+const p2 = document.querySelector('section#a .p2');
