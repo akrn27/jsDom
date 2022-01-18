e37d1c0447fd3370e4395850d0160737289c1116
Akrn27 <akurnia755@gmail.com>
2022-01-18 14:21:55 +0700

    finish addEventListener

--- js/jsDom7.js
+++ js/jsDom7.js
@@ -1,15 +1,29 @@
-const p3 = document.querySelector('.p3');
+// const p3 = document.querySelector('.p3');
 
 
-ubahWarnaP2 = () => {
-    p2.style.backgroundColor = 'lightBlue';
-}
+// ubahWarnaP2 = () => {
+//     p2.style.backgroundColor = 'lightBlue';
+// }
 
-ubahWarnaP3 = () => {
-    p3.style.backgroundColor = 'lightBlue';
-}
+// ubahWarnaP3 = () => {
+//     p3.style.backgroundColor = 'lightBlue';
+// }
 
-const p2 = document.querySelector('.p2');
-p2.onclick = ubahWarnaP2;
+// const p2 = document.querySelector('.p2');
+// p2.onclick = ubahWarnaP2;
+
+
+// const p4 = document.querySelector('section#b p');
+// p4.addEventListener('click', function(){
+//     const prompt1 = prompt('Masukkan Nilai: ');
+
+//     const liBaru = document.createElement('li');
+//     const liTextBaru = document.createTextNode(prompt1);
+//     liBaru.appendChild(liTextBaru);
+
+
+//     const li = document.querySelector('section#b ul');
+//     li.appendChild(liBaru);
+// });
 
 
