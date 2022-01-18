39947e2536415f971b14ff94af2c9ea47b84754b
Akrn27 <akurnia755@gmail.com>
2022-01-18 13:49:07 +0700

    finish onclick event

--- js/jsDom7.js
+++ js/jsDom7.js
@@ -1,4 +1,15 @@
-bgChange = () => {
-    const p3 = document.querySelector('.p3');
+const p3 = document.querySelector('.p3');
+
+
+ubahWarnaP2 = () => {
+    p2.style.backgroundColor = 'lightBlue';
+}
+
+ubahWarnaP3 = () => {
     p3.style.backgroundColor = 'lightBlue';
 }
+
+const p2 = document.querySelector('.p2');
+p2.onclick = ubahWarnaP2;
+
+
